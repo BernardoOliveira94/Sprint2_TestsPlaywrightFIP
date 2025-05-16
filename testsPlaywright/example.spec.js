3edd175c0147f5b6b6e9d1bd912d93e7876b40c8
BernardoOliveira94 <73598916+BernardoOliveira94@users.noreply.github.com>
2025-05-16 22:30:45 +0100

    commit 4 password reset done

--- testsPlaywright/example.spec.js
+++ testsPlaywright/example.spec.js
@@ -7,6 +7,9 @@ test('has title', async ({ page }) => {
   // Expect a title "to contain" a substring.
   //await expect(page).toHaveTitle(/FIP/);
 
+
+  //TEST 1 Login
+
   //Insert email
   const email = page.getByRole('textbox', { name: 'Email institucional' });
   await email.fill('b.m.oliveira94@gmail.com');
@@ -21,6 +24,10 @@ test('has title', async ({ page }) => {
 
   //Wait for the login
   await page.waitForTimeout(1000);
+  
+
+/*
+  //TEST 2 Create a PDD
 
   //Click in dashboard
   const dashboard = page.getByRole('link', { name: 'PDD PDD Dashboard' });
@@ -31,14 +38,49 @@ test('has title', async ({ page }) => {
   await newPDD.click();
 
   //Click in Select subject
-  const selectOption1 = page.getByLabel('Disciplina');
-  await selectOption1.click();
+  const selectSubject = page.getByLabel('Disciplina');
+  await selectSubject.click();
+
+  //Wait for the dropdown
+  await page.waitForTimeout(1000);
 
   //Select the subject by value, label or index
-  await page.selectOption('#Disciplina', '11'); // value
-  //await page.selectOption('#Disciplina', { label: '11' }); //label
+  //await page.selectOption('#Disciplina', '11'); // value
+  //await page.selectOption('#Disciplina', { label: '11 - DISCIPLINA ONZE' }); //label
   //await page.selectOption('#Disciplina', { index: 0 }); //index
+*/
 
+/*
+  //TEST 3 Password reset
 
+  //Select profile name
+  const clickProfileName = page.getByRole('link', { name: 'Profile BERNARDO' });
+  await clickProfileName.click();
+
+  //Select profile option
+  const clickProfileOption = page.getByRole('link', { name: ' Perfil' });
+  await clickProfileOption.click();
+
+  //Wait for the next page
+  await page.waitForTimeout(1000);
+
+  //Click on password edit
+  const clickPassEdit = page.getByRole('link', { name: 'Editar Senha' });
+  await clickPassEdit.click();
+
+  //Fill new password
+  const newPassword1 = page.getByRole('textbox', { name: 'Nova senha' });
+  await newPassword1.fill('Bo*1234567');
+
+  //Repeat fill new password
+  const newPassword2 = page.getByRole('textbox', { name: 'Repita a senha' });
+  await newPassword2.fill('Bo*1234567');
+
+  //Select reset password
+  const clickResetPassword = page.getByRole('button', { name: 'Redefinir Senha' });
+  await clickResetPassword.click();
+*/
+
+  //TEST 4
 
 });
