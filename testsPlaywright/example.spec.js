4cd520e6eda6f8308cb2710fa5bf3d9e850b1e44
BernardoOliveira94 <73598916+BernardoOliveira94@users.noreply.github.com>
2025-05-16 21:43:00 +0100

    commit 3 selecao dropdown
    em progresso...

--- testsPlaywright/example.spec.js
+++ testsPlaywright/example.spec.js
@@ -19,4 +19,26 @@ test('has title', async ({ page }) => {
   const login = page.getByRole('button', { name: 'Entrar' });
   await login.click();
 
+  //Wait for the login
+  await page.waitForTimeout(1000);
+
+  //Click in dashboard
+  const dashboard = page.getByRole('link', { name: 'PDD PDD Dashboard' });
+  await dashboard.click();
+
+  //Click in New PDD
+  const newPDD = page.getByRole('link', { name: 'Novo PDD PDD' });
+  await newPDD.click();
+
+  //Click in Select subject
+  const selectOption1 = page.getByLabel('Disciplina');
+  await selectOption1.click();
+
+  //Select the subject by value, label or index
+  await page.selectOption('#Disciplina', '11'); // value
+  //await page.selectOption('#Disciplina', { label: '11' }); //label
+  //await page.selectOption('#Disciplina', { index: 0 }); //index
+
+
+
 });
